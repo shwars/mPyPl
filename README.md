251d7d2bd0c8359cf4505d5d1841e12b1b5d358f
Dmitri Soshnikov <dmitri@soshnikov.com>
2018-10-30 21:46:07 +0300

    Add setup / readme / license

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mPyPl
-Monadic Pipeline Library for Python
+
+## Monadic Pipeline Library for Python
+
+This library was created by a team of enthusiastic software developers / data scientists at Microsoft, who
+wanted to simplify tasks of data processing and creating complex data pipelines. The library is inspired
+by the following main ideas:
+
+ * Using functional approach to data processing (which implies immutability, lazy evaluation, etc.) 
+ * Using [pipe](https://github.com/JulienPalard/Pipe) module in Python to achieve data pipelines similar to 
+   [F#](http://fsharp.org).
+ * Data pipeline uses dictionaries with different fields as base type, new operations would typically enrich data and add 
+   new fields by using `apply` function. Those dictionaries are similar to *monads*, and `apply` is similar to *lift* operation
+   on monads. Thus the naming of the library.
+   
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setuptools.setup(
+    name='mPyPl',
+    packages=setuptools.find_packages(),
+    version='0.0.1',
+    description='Monadic Pipeline Library for Python',
+    author='Dmitri Soshnikov',
+    author_email='dmitri@soshnikov.com',
+    url='https://github.com/shwars/mPyPl',
+#    download_url='https://github.com/JulienPalard/Pipe/tarball/master',
+    long_description=readme,
+    long_description_content_type='text/markdown; charset=UTF-8',
+    license='MIT license',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+#        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
