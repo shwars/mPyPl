5c222f205c1838c8c92bb286982ad9bf83119322
Dmitri Soshnikov <dmitri@soshnikov.com>
2019-07-06 00:43:22 +0300

    Add encoding param to csvsource

--- mPyPl/sources.py
+++ mPyPl/sources.py
@@ -4,15 +4,14 @@
 from .mdict import *
 import csv
 
-def csvsource(fn,sep=','):
+def csvsource(fn,sep=',',encoding=None):
     """
     Create a data source from CSV file
     :param fn: Filename
     :param sep: Separator to use. Defaults to ','
     :return: Sequence of mdict object representing CSV data. Field names are taken from first line
     """
-    with open(fn, newline='') as csvfile:
+    with open(fn, newline='',encoding=encoding) as csvfile:
         reader = csv.DictReader(csvfile,delimiter=sep)
         for row in reader:
             yield mdict(row)
-
